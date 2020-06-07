e65180270719c0f6f4d1248d9913cc6274ad2ee3
Alejandro Bernal <abernalb@correo.udistrital.edu.co>
2020-06-06 23:19:26 -0500

    Estilos aplicados

--- Index.php
+++ Index.php
@@ -5,19 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="shortcut icon" href="#" />      
     <link rel="stylesheet" href="bootstrap4/css/bootstrap.min.css">  
+    <link rel="stylesheet" href="estilos.css">
     <title>Nomina</title>
 </head>
-<style>
-table, th, td {
-  border: 1px solid black;
-  border-collapse: collapse;
-}
-th, td {
-  padding: 5px;
-  text-align: left;    
-}
-</style>
 <body>
+    
+    <header style="height: 70px">
+     </header> 
     <?php
         session_start();
         ob_start();
@@ -102,102 +96,161 @@ th, td {
         }
         
     ?>
-<form  method="POST">
-    <h1 align="center">Nomina Mensual</h1>
+<div style="height: 30px;"></div>
+<div class="container">
+    <div class="row">
+    <div class="col-lg-12">
+    <div class="card shadow-lg p-3 mb-5 bg-white ">
+    <div class="card-header">Nomina mensual </div>
+    <div class="card-body">    
+<form  method="POST" class="needs-validation" novalidate >
+    
     <?php
         
         if(!isset($_SESSION['Salario_minimo']))
         {
             ECHO '
-            <div class="card" style="width: 25rem;">
-            <h2>Valores de referencia</h2>
-            <div class="form-group" >
-            <label for="Salario_minimo">Salario minimo</label>
-            <input type="number" id="Salario_minimo" name="Salario_minimo" class="form-control" require >
-            </div>
-            <div class="form-group" >
-            <label for="Auxilio_Transporte">Auxilio de transporte</label>
-            <input type="number" id="Auxilio_Transporte" name="Auxilio_Transporte"  class="form-control" require >
-            </div>
-            <div class="form-group" >
-            <label for="Exoneracion">¿Está exonerado del pago de aportes?</label>
-            <select name="Exoneracion" id="Exoneracion" class="form-control">
-                <option value=Si>Si</option>
-                <option value=No>No</option>
+            <h3>Valores de referencia</h3>
+            <div class="form-row">
                 
-           </select>
-           </div>
-           <button type="submit" name="Agregar" class="btn btn-success"> Actualizar </button>
-           </div>
+                <div class="col-md-4 mb-3" >
+                    <label for="Salario_minimo">Salario minimo</label>
+                    <input type="number" id="Salario_minimo" name="Salario_minimo" class="form-control" require >
+                </div>
+                <div class="col-md-4 mb-3" >
+                    <label for="Auxilio_Transporte">Auxilio de transporte</label>
+                    <input type="number" id="Auxilio_Transporte" name="Auxilio_Transporte"  class="form-control" require >
+                </div>
+        
+                <div class="col-md-4 mb-3" >       
+                    <label for="Exoneracion">¿Está exonerado del pago de aportes?</label>
+                    <select name="Exoneracion" id="Exoneracion" class="form-control">
+                        <option value=Si>Si</option>
+                        <option value=No>No</option>
+                    </select>
+                </div> 
+               
+            </div>
+            
+                <button type="submit" name="Agregar" class="btn btn-primary"> Actualizar </button>   
+                
            '
 
            ;
         }
         else{
            ECHO'
-           <h2>Valores de referencia</h2>
-            <div class="form-group" >
-                <label for="Salario_minimo">Salario minimo</label>
-                <input type="text" id="Salario_minimo" name="Salario_minimo" value = "'.$_SESSION['Salario_minimo'].'" disabled>
-            </div>
-            <div class="form-group" >
-                <label for="Auxilio_Transporte">Auxilio de transporte</label>
-                <input type="text" id="Auxilio_Transporte" name="Auxilio_Transporte" value ="'.$_SESSION['Auxilio_Transporte'].'" disabled>
-            </div>
-            <div class="form-group" >
-                <label for="Exoneracion">¿Está exonerado del pago de aportes?</label>
-                <select name="Exoneracion" id="Exoneracion"  disabled>
-                    <option value=1>'.$_SESSION['Exoneracion'].'</option>
-                    
-                </select>
+           <h3>Valores de referencia</h3>
+            <div class="form-row">
+                <div class="col-md-4 mb-3"  >
+                    <label for="Salario_minimo">Salario minimo</label>
+                    <input type="text" id="Salario_minimo" name="Salario_minimo" value = "'.$_SESSION['Salario_minimo'].'" class="form-control" disabled>
+                </div>
+                <div class="col-md-4 mb-3"  >
+                    <label for="Auxilio_Transporte">Auxilio de transporte</label>
+                    <input type="text" id="Auxilio_Transporte" name="Auxilio_Transporte" value ="'.$_SESSION['Auxilio_Transporte'].'" class="form-control" disabled>
+                </div>
+                <div class="col-md-4 mb-3"  >
+                    <label for="Exoneracion">¿Está exonerado del pago de aportes?</label>
+                    <select name="Exoneracion" id="Exoneracion"  disabled class="form-control" >
+                        <option value=1>'.$_SESSION['Exoneracion'].'</option>
+                        
+                    </select>
+                 </div>
             </div>';     
         }
      ?>   
-    </div><br>
-    <div   style="border-style: double;">
-        <label for="Id_Empleado">Identificacion del empleado</label>
-        <input type="text" id="Id_Empleado" name="Id_Empleado" require>
-        <label for="Nombre_Empleado">Nombre del empleado</label>
-        <input type="text" id="Nombre_Empleado" name="Nombre_Empleado" require>
-        <label for="Salario_Empleado">Salario Basico</label>
-        <input type="number" id="Salario_Empleado" name="Salario_Empleado" require>
-        <label for="Dias_liquidados">Dias liquidados</label>
-        <input type="number" id= "Dias_liquidados" name="Dias_liquidados" max="30" min = "1" require><br>
-        <label for="Horas_diurnas">Horas extras diurnas</label>
-        <input type="number" id= "Horas_diurnas" name="Horas_diurnas" max="48" min = "0" require><br>
-        <label for="Horas_nocturnas">Horas extras nocturnas</label>
-        <input type="number" id= "Horas_nocturnas" name="Horas_nocturnas" max="48" min = "0" require><br>
-        <label for="Horas_dominicales">Horas extras dominicales</label>
-        <input type="number" id= "Horas_dominicales" name="Horas_dominicales" max="48" min = "0" require><br>
-        <button type="submit" name="Insertar"> Insetar </button>    
+    <h3>Ingreso empleado</h3>
+    <div class="form-row">
 
+        <div class="col-md-4 mb-3"  >
+            <label for="Id_Empleado">Identificacion del empleado</label>
+            <input type="text" id="Id_Empleado" name="Id_Empleado"  class="form-control" require>
+        </div>
+        <div class="col-md-4 mb-3"  >
+            <label for="Nombre_Empleado">Nombre del empleado</label>
+            <input type="text" id="Nombre_Empleado" name="Nombre_Empleado" class="form-control" require>
+        </div>
+        <div class="col-md-4 mb-3"  >
+            <label for="Salario_Empleado">Salario Basico</label>
+            <input type="number" id="Salario_Empleado" name="Salario_Empleado" class="form-control" require>
+        </div>    
     </div>
-    <div "border-style: double;">
-        <table style="width:100%">
-            <tr>
-                <th rowspan="2">Identificacion</th>
-                <th rowspan="2" >Nombre </th>
-                <th colspan="8">Devengado</th>
-                <th colspan="5">Deducciones </th>
-                <th rowspan="2">Neto pagado</th>
-                
-            </tr>
-            <tr>
-                <td>Salario básico</td>
-                <td>Días liquidados</td>
-                <td>Salario devengado</td>
-                <td>Horas extras</td>
-                <td>Recargos nocturnos</td>
-                <td>Trabajo dominical y festivo</td>
-                <td>Auxilio de transporte</td>
-                <td>Total devengado</td>
-                <td>Salud</td>
-                <td>Pensión</td>
-                <td>Fondo de solidaridad</td>
-                <td>Retencion de fuente</td>
-                <td>Otras deducciones</td>
-                
-            </tr>
+    <div class="form-row">    
+        <div class="col-md-4 mb-3"  >
+            <label for="Dias_liquidados">Dias liquidados</label>
+            <input type="number" id= "Dias_liquidados" name="Dias_liquidados" max="30" min = "1" class="form-control" require>
+        </div>
+        <div class="col-md-4 mb-3"  >
+            <label for="Horas_diurnas">Horas extras diurnas</label>
+            <input type="number" id= "Horas_diurnas" name="Horas_diurnas" max="48" min = "0" class="form-control" require>
+        </div>
+        <div class="col-md-4 mb-3"  >
+            <label for="Horas_nocturnas">Horas extras nocturnas</label>
+            <input type="number" id= "Horas_nocturnas" name="Horas_nocturnas" max="48" min = "0" class="form-control" require>
+        </div>
+    </div>
+    <div class="form-row">  
+        <div class="col-md-4 mb-3"  >  
+            <label for="Horas_dominicales">Horas extras dominicales</label>
+            <input type="number" id= "Horas_dominicales" name="Horas_dominicales" max="48" min = "0" class="form-control" require>
+        </div>
+    </div>        
+        <button type="submit" name="Insertar" class="btn btn-primary" > Insertar </button>
+    
+          
+          
+    <button type="submit" name="Reiniciar" class="btn btn-danger"> Reiniciar </button> 
+        
+</form>
+</div>   
+    </div>
+            </div>       
+        </div>                  
+    </div>
+<div>
+<div style="height: 50px;"></div>  
+<div class="container">
+    <div class="row">
+    <div class="col-lg-12">
+    <div class="card shadow-lg p-3 mb-5 bg-white ">
+    <div class="card-header">Nomina mensual </div>
+    <div class="card-body"> 
+<style>
+    th{
+        text-align: center;
+    }
+    td{
+        text-align: right;
+    }
+</style>        
+<table  valign="middle" border="1" cellspacing="0" cellpadding="0" style="font-size:13px" style="text-align:center;" align="center" >
+            <thead>
+                <tr>
+                    <th  rowspan="2"  >Identificacion</th>
+                    <th  rowspan="2"  >Nombre </th>
+                    <th  colspan="8"  >Devengado</th>
+                    <th  colspan="5" > Deducciones </th>
+                    <th  rowspan="2" >Neto pagado</th>
+                    
+                </tr>
+                <tr>
+                    <th style="text-align:center;">Salario básico</td>
+                    <th >Días liquidados</td>
+                    <th >Salario devengado</td>
+                    <th >Horas extras</td>
+                    <th >Recargos nocturnos</td>
+                    <th >Trabajo dominical y festivo</td>
+                    <th >Auxilio de transporte</td>
+                    <th >Total devengado</td>
+                    <th >Salud</td>
+                    <th >Pensión</td>
+                    <th >Fondo de solidaridad</td>
+                    <th >Retencion de fuente</td>
+                    <th >Otras deducciones</td>
+                    
+                </tr>
+            </thead>    
             <?php
                 if(!isset($_SESSION['Salario_minimo']))
                 {
@@ -227,12 +280,13 @@ th, td {
                     }
                 }
             ?>
-        </table>    
-    </div>       
-    <button type="submit" name="Reiniciar"> Reiniciar </button> 
-        
-</form>
-
+        </table>  
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>    
 </body>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
